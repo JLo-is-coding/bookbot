78f0d83780075d914cbe89aa224c3c4ab958e2f6
jlo2001 <Jacklowes2001@gmail.com>
2025-06-28 19:17:15 +0100

    add word counter and file fetch to main

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    file_contents = (get_book_text("books/frankenstein.txt"))
+    num_words = count_words(file_contents)
+    print (f"{num_words} words found in the document")
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+def count_words(text):
+    words = text.split()
+    word_count = 0
+    for word in words:
+        word_count += 1
+    return word_count
+
+main()
