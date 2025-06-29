5294539450f0979b0ebe1ce9d916db154de6b256
jlo2001 <Jacklowes2001@gmail.com>
2025-06-29 01:45:34 +0100

    added sys input support

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from stats import count_words, count_characters, sort_characters
+import sys
 
 def main():
-    file_contents = (get_book_text("books/frankenstein.txt"))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_contents = (get_book_text(sys.argv[1]))
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     num_words = count_words(file_contents)
     print(f"Found {num_words} total words")
